a6cf3e01eff996b41df64f6f306a5d97836a44d6
1savmoney <w43308682@gmail.com>
2025-01-22 15:55:45 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
-function redirectTo(url) {
-    window.location.href = url;
+function handleRedirect(discountCode, targetUrl) {
+    // 將折扣碼複製到剪貼簿
+    const tempInput = document.createElement("input");
+    document.body.appendChild(tempInput);
+    tempInput.value = discountCode;
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+
+    // 跳轉到目標網址
+    window.location.href = targetUrl;
 }
